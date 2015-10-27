a801b78c6f6fae9acf0c1fc866526001d986bb7b
alhanger <hanger129@gmail.com>
2015-10-27 15:18:54 -0400

    Add users hashmap, logout and delete post features

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -1,6 +1,7 @@
 package com.theironyard;
 
 import spark.ModelAndView;
+import spark.Session;
 import spark.Spark;
 import spark.template.mustache.MustacheTemplateEngine;
 
@@ -10,22 +11,27 @@ import java.util.HashMap;
 public class Main {
 
     public static void main(String[] args) {
-        ArrayList<Post> posts = new ArrayList<>();
-        User currentUser = new User();
-        Spark.staticFileLocation("/public");
+        HashMap<String, User> users = new HashMap<>();
         Spark.init();
         Spark.post(
                 "/create-user",
                 ((request, response) -> {
+                    Session session = request.session();
+
                     String name = request.queryParams("username");
                     String pw = request.queryParams("password");
-                    if (currentUser.name == null) {
-                        currentUser.name = name;
-                        currentUser.password = pw;
-                        response.redirect("/posts");
+
+                    session.attribute("username", name);
+
+                    if (users.get(name) == null) {
+                        User user1 = new User();
+                        user1.name = name;
+                        user1.password = pw;
+                        users.put(name, user1);
+                        response.redirect("/");
                     }
-                    else if (pw.equals(currentUser.password) && name.equals(currentUser.name)) {
-                        response.redirect("/posts");
+                    else if (pw.equals(users.get(name).password) && name.equals(users.get(name).name)) {
+                        response.redirect("/");
                     }
                     else {
                         return "There was an error";
@@ -36,23 +42,62 @@ public class Main {
         Spark.post(
                 "/create-post",
                 ((request, response) -> {
+                    Session session = request.session();
+                    String name = session.attribute("username");
                     String post = request.queryParams("userPost");
-                    Post newPost = new Post();
-                    newPost.text = post;
-                    posts.add(newPost);
-                    response.redirect("/posts");
+
+                    if (!post.isEmpty()) {
+                        Post newPost = new Post();
+                        newPost.id = users.get(name).posts.size() + 1;
+                        newPost.text = post;
+                        users.get(name).posts.add(newPost);
+                    }
+                    response.redirect("/");
                     return "";
                 })
         );
         Spark.get(
-                "/posts",
+                "/",
                 ((request, response) -> {
+                    Session session = request.session();
+                    String username = session.attribute("username");
+                    if (username == null) {
+                        return new ModelAndView(new HashMap(), "not-logged-in.html");
+                    }
                     HashMap m = new HashMap();
-                    m.put("user", currentUser.name);
-                    m.put("posts", posts);
-                    return new ModelAndView(m, "posts.html");
+                    m.put("user", username);
+                    m.put("posts", users.get(username).posts);
+                    return new ModelAndView(m, "logged-in.html");
                 }),
                 new MustacheTemplateEngine()
+        );
+        Spark.post(
+                "/logout",
+                ((request, response) -> {
+                    Session session = request.session();
+                    session.invalidate();
+                    response.redirect("/");
+                    return "";
+                })
+        );
+        Spark.post(
+                "/delete-post",
+                ((request, response) -> {
+                    Session session = request.session();
+                    String name = session.attribute("username");
+                    String id = request.queryParams("postid");
+                    try {
+                        int idNum = Integer.valueOf(id);
+                        users.get(name).posts.remove(idNum - 1);
+                        for (int i = 0; i < users.get(name).posts.size(); i++) {
+                            users.get(name).posts.get(i).id = i + 1;
+                        }
+                    } catch (Exception e) {
+
+                    }
+                    response.redirect("/");
+                    return "";
+                })
         );
     }
 }
--- src/com/theironyard/User.java
+++ src/com/theironyard/User.java
@@ -1,9 +1,12 @@
 package com.theironyard;
 
+import java.util.ArrayList;
+
 /**
  * Created by alhanger on 10/26/15.
  */
 public class User {
     String name;
     String password;
+    ArrayList<Post> posts = new ArrayList<>();
 }
